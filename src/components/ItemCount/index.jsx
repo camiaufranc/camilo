988646fc78f85651350dbbfb44e45a3e5b49cc96
camiaufranc <camiloaufranc7@gmail.com>
2022-07-18 23:57:58 -0300

    new component

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,35 @@
+import './itemCount.css';
+
+import React, { /* useEffect,  */useState } from "react";
+
+export const ItemCount = (initial, stock , onAdd) => {
+    // const [count, setcount] = useState(parseInt(initial))
+    const [count, setCount] = useState(initial)
+
+    const decrease = () => {
+        setCount (count - 1)
+        
+    }
+
+    const increase = () => {
+        setCount (count + 1)
+    }
+
+/*     useEffect (() => {
+        setcount (parseInt(initial))
+    } , [initial] ) */
+
+    return ( 
+    <div className='counter'>
+        <button disabled= {count <= 1} onClick={decrease}>-</button>
+        <span>{ count }</span>
+        <button disabled= {count >= stock} onClick={increase}>+</button>
+        <div>
+            <button /* disabled= {count <= 0} onClick= { () => onAdd(count)} */ >Agregar al carrito</button>
+        </div>
+    </div>
+
+    );
+}
+
+export default ItemCount;
