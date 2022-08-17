5ceed6276c1acb727ef9e5b16d8fe27c37d8d04a
camiaufranc <camiloaufranc7@gmail.com>
2022-08-17 10:20:31 -0300

    ult entrega

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
-import { useCartContext } from "../../Context/CartContext.jsx";
-import ItemCart from '../ItemCart';
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useCartContext } from "../../Context/CartContext";
+import ItemCart from "../ItemCart";
 
 const Cart = () => {
-    const { cart, totalPrice } = useCartContext();
+	const { cart, totalPrice } = useCartContext();
 
-    if(cart.length === 0) {
-        return (
-            <>
-                {
-                    cart.map(product => <ItemCart key={product.id} product={product}/>)
-                }
+	const order = {
+		buyer: {
+			name: "Camilo",
+			email: "camilo@gmail.com",
+			phone: "123123",
+			address: "asdd",
+		},
+		items: cart.map((product) => ({
+			id: product.id,
+			title: product.title,
+			price: product.price,
+			quantity: product.quantity,
+		})),
+		total: totalPrice(),
+	};
 
-                <p>
-                    total: {totalPrice()}
-                </p>                
-            </>
-        );
-    }
-    
-    return (
-        <div>Cart</div>
-    )
-}
+	const handleClick = () => {
+		const db = getFirestore();
+		const ordersCollection = collection(db, "orders");
+		addDoc(ordersCollection, order).then(({ id }) => console.log(id));
+	};
+
+	if (cart.length === 0) {
+		return (
+			<>
+				<p>No hay elementos en el carrito</p>
+				<Link to="/">Hacer compras</Link>
+			</>
+		);
+	}
+
+	return (
+		<>
+			{cart.map((product) => (
+				<ItemCart key={product.id} product={product} />
+			))}
+			<p>total: {totalPrice()}</p>
+			<button onClick={handleClick}>Emitir compra</button>
+		</>
+	);
+};
 
 export default Cart;
